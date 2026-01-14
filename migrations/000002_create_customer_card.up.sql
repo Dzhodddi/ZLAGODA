6a8b2a1e566993994774a5e1b3181d679abda8a8
Dzzhodddi <69247957+Dzhodddi@users.noreply.github.com>
2026-01-14 23:26:34 +0200

    Add migrations for category and customer card

---
+++ migrations/000002_create_customer_card.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS customer_card(
+    card_number BIGSERIAL PRIMARY KEY,
+    customer_surname VARCHAR(50) NOT NULL,
+    customer_name VARCHAR(50) NOT NULL,
+    customer_patronymic VARCHAR(50),
+    phone_number VARCHAR(13) NOT NULL,
+    city VARCHAR(50),
+    street VARCHAR(50),
+    zip_code VARCHAR(9),
+    customer_percent INT NOT NULL
+);
