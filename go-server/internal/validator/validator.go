ef3bf58bbb0073f19131486b723be95cbb0deb29
Dzzhodddi <69247957+Dzhodddi@users.noreply.github.com>
2026-01-16 14:44:32 +0200

    Remove redundant code

--- go-server/internal/validator/validator.go
+++ go-server/internal/validator/validator.go
@@ -1,9 +1,6 @@
 package validation
 
 import (
-	"reflect"
-
-	"github.com/Dzhodddi/ZLAGODA/internal/types"
 	"github.com/go-playground/validator/v10"
 )
 
@@ -11,12 +8,6 @@ var validate *validator.Validate
 
 func init() {
 	validate = validator.New(validator.WithRequiredStructEnabled())
-	validate.RegisterCustomTypeFunc(func(field reflect.Value) interface{} {
-		if d, ok := field.Interface().(types.Date); ok {
-			return d.Time()
-		}
-		return nil
-	}, types.Date{})
 }
 
 func ValidateStruct(s interface{}) error {
