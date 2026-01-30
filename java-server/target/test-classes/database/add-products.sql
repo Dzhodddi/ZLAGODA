a2af4680bccca3146d1edd633cec72d3344409e2
kastashenkova <astashenkova.katya@gmail.com>
2026-01-30 05:47:50 +0200

    delete target from git

--- java-server/target/test-classes/database/add-products.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO product (id_product, category_number, product_name, product_characteristics)
-VALUES
-    (1, 1, 'Milk', '1L, low-fat'),
-    (2, 1, 'Bread', '500g, wheat'),
-    (3, 2, 'Soap', '100g, moisturizing');
