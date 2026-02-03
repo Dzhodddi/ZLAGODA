2c7c47b55bbb0ade3eb3c364f5e40b6405eb2de0
kastashenkova <astashenkova.katya@gmail.com>
2026-02-03 23:02:02 +0200

    fix deletion for product and store product

--- java-server/target/generated-sources/annotations/org/example/mapper/employee/EmployeeMapperImpl.java
+++ java-server/target/generated-sources/annotations/org/example/mapper/employee/EmployeeMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-01-28T16:58:43+0200",
+    date = "2026-02-03T23:01:16+0200",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 23.0.2 (Oracle Corporation)"
 )
 @Component
