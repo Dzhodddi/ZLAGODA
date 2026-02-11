fc8144a218bc0cf9c4fee0ca1e8b499d5dba0ea9
kastashenkova <astashenkova.katya@gmail.com>
2026-02-11 14:20:41 +0200

    add java healthcheck

--- java-server/src/main/java/org/example/config/SecurityConfig.java
+++ java-server/src/main/java/org/example/config/SecurityConfig.java
@@ -47,7 +47,8 @@ public class SecurityConfig {
                                 .requestMatchers("/swagger-ui/**",
                                         "/v3/api-docs/**",
                                         "/auth/**",
-                                        "/error")
+                                        "/error",
+                                        "/actuator/health")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
