4d68c88a501f4c22c4df8667a2cdb10f22377388
Dzzhodddi <69247957+Dzhodddi@users.noreply.github.com>
2026-02-16 21:05:23 +0200

    Add types for customer card

--- web/src/features/customer-card/types/types.ts
+++ web/src/features/customer-card/types/types.ts
@@ -1,3 +1,20 @@
 import {z} from 'zod';
 
-export const CreateCustomerCard = z.
+const phoneRegex = new RegExp(
+    /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/
+);
+
+
+export const CreateCustomerCard = z.object({
+    CardNumber: z.string().min(1).max(13),
+    CustomerName: z.string().min(1).max(50),
+    CustomerSurname: z.string().min(1).max(50),
+    CustomerPatronymic: z.optional(z.string().min(1).max(50)),
+    PhoneNumber: z.string().min(1).max(13).regex(phoneRegex, 'Invalid phone number'),
+    City: z.optional(z.string().min(1).max(50)),
+    Street: z.optional(z.string().min(1).max(50)),
+    ZipCode: z.optional(z.string().min(1).max(9)),
+    CustomerPercent: z.number().min(0).max(100),
+});
+
+export type CreateCustomerCard = z.infer<typeof CreateCustomerCard>;
