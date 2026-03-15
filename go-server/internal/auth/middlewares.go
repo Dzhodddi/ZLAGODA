a6420601d2972b103f6eaaf93b4bda7599700d44
dzzoddii <d.dzhos@ukma.edu.ua>
2026-03-15 17:35:35 +0200

    Remove unused import

--- go-server/internal/auth/middlewares.go
+++ go-server/internal/auth/middlewares.go
@@ -11,7 +11,6 @@ import (
 	repository "github.com/Dzhodddi/ZLAGODA/internal/repositories"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
-	"github.com/labstack/gommon/log"
 )
 
 type employeeKey string
